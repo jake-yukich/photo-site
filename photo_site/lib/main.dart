20c84dff86319bcd6354789f55983af7381b9b92
jake-yukich <jake.yukich@gmail.com>
2025-01-07 11:14:17 -0800

    project init

---
+++ photo_site/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const PhotoApp());
+}
+
+class PhotoApp extends StatelessWidget {
+  const PhotoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photography Portfolio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.grey,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+} 
